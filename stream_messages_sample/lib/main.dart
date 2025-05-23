c7a544446ebe79531d7da1503803bcf0756d01c0
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-05-23 08:05:56 -0500

    Replace default widget with new MessagePage

--- stream_messages_sample/lib/main.dart
+++ stream_messages_sample/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:stream_messages_sample/Widgets/message_list_view.dart';
+import 'package:stream_messages_sample/Widgets/message_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,20 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      home: Scaffold(
-        body: SafeArea(
-          minimum: EdgeInsets.only(left: 16, right: 16),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Center(child: Text("Stream Messages Sample")),
-              SizedBox(height: 20),
-              Expanded(child: MessageListView()),
-            ],
-          ),
-        ),
-      ),
-    );
+    return CupertinoApp(home: MessagePage());
   }
 }
