fa4c64cdaad5f51c3fc713a18ed789eeff3c3502
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-05-28 13:21:05 -0500

    Add lifecycle listener to main app and initialize bg tasks

--- local_persistence_form_sample/lib/Widgets/form_page.dart
+++ local_persistence_form_sample/lib/Widgets/form_page.dart
@@ -5,20 +5,15 @@ import 'package:local_persistence_form_sample/Core/user_repository_abstract.dart
 import 'package:local_persistence_form_sample/Data/DTO/user_model.dart';
 import 'package:local_persistence_form_sample/Widgets/user_form.dart';
 
-class FormPage extends StatefulWidget {
+class FormPage extends StatelessWidget {
   const FormPage({super.key, required this.repository});
 
   final SqfliteUserRepository repository;
 
-  @override
-  State<FormPage> createState() => _FormPageState();
-}
-
-class _FormPageState extends State<FormPage> {
   Future<Result<UserModel, SqfliteStackError>> _onSubmit(
     Map<String, Object?> formFields,
   ) async {
-    final result = await widget.repository.save(formFields);
+    final result = await repository.save(formFields);
     return result;
   }
 
--- local_persistence_form_sample/lib/main.dart
+++ local_persistence_form_sample/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_persistence_form_sample/Core/background_tasks.dart';
 import 'package:local_persistence_form_sample/Core/sqflite_stack.dart';
 import 'package:local_persistence_form_sample/Data/user_repository.dart';
 import 'package:local_persistence_form_sample/Widgets/form_page.dart';
@@ -6,11 +7,43 @@ import 'package:local_persistence_form_sample/Widgets/form_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SqfliteStack.instance.init();
+  BackgroundTasks.instance.cleanDB(UserRepository());
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final AppLifecycleListener _lifecycleListener;
+
+  @override
+  initState() {
+    _lifecycleListener = AppLifecycleListener(
+      onShow: () => debugPrint("Showing onShow"),
+      onResume: () => debugPrint("Resuming onResume"),
+      onRestart: () => debugPrint("Restarting onRestart"),
+      onPause: () => debugPrint("Pausing onPause"),
+      onHide: () => debugPrint("Hiding onHide"),
+      onInactive: () async {
+        debugPrint("Deactivating onInactive");
+        BackgroundTasks.instance.scheduleCleanTask();
+      },
+      onDetach: () => debugPrint("Detaching onDetach"),
+      onStateChange: (state) => setState(() {}),
+    );
+    super.initState();
+  }
+
+  @override
+  dispose() {
+    _lifecycleListener.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
